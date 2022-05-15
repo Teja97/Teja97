06d7f4c92587c3997ec7206ace1d130a816a1212
Teja <upperpalliteja@gamil.com>
2022-05-15 14:07:48 +0530

    Intiail commit

---
+++ 01_list.py
@@ -0,0 +1,23 @@
+# Creating a list using []
+a = [21, 22, 3, 45]  # print(a[2]) , print(a)....
+
+# Changing values in list
+# a[2] = 45
+
+# we can create list with items of different type
+c = ["bRAIN", False, 1.3, 4]
+
+# List slicing , print(c[0:4]) , print(c[-4:])
+
+# List Methods
+# 1. sort()   2. reverse()   3. append()   4. insert()  5. pop()   6. remove()   ......
+# a.sort()
+# a.reverse()
+# a.append(66) # adds at the end of the list, after adding 66 we can add one more item and .... we can add no.of items
+# a.insert(1, 5) # inserts 5 at index 1
+# which means by using it we can insert the value at any place in the list, then we can see changes in index
+# a.pop(2) # removes element from index 2 which is 22
+# a.remove(21) # removes directly the element like 21
+print(a)
+
+
