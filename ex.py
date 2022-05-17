977b69aebdf700b4f62159f3cbf881e8f38b47d3
Teja <upperpalliteja@gamil.com>
2022-05-17 11:43:49 +0530

    created a game win program

--- ex.py
+++ ex.py
@@ -1,2 +1,44 @@
-a = 1
-print(a)
+import random
+
+
+def gw(comp, u):
+    if comp == u:
+        return None
+    if comp == "s":
+        if u == "w":
+            return False
+        elif u == "g":
+            return True
+    if comp == "w":
+        if u == "g":
+            return False
+        elif u == "s":
+            return True
+    if comp == "g":
+        if u == "s":
+            return False
+        elif u == "w":
+            return True
+
+
+print("comp Turn snake(s), water(w), gun(g)")
+r = random.randint(1, 3)
+if r == 1:
+    comp = "s"
+elif r == 2:
+    comp = "w"
+elif r == 3:
+    comp = "g"
+
+u = input("Choose any one of this option s,w,g : ")
+a = gw(comp, u)
+print(f"computer chose {comp} and your chose {u}")
+if a is None:
+    print("The game is tie")
+elif a:
+    print("you win!")
+else:
+    print("you loss!")
+
+
+
