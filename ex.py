268714d0ebf3de67fe6461f3a5fd81c589ba66ba
Teja <upperpalliteja@gamil.com>
2022-07-07 15:25:09 +0530

    added and modified the data

--- ex.py
+++ ex.py
@@ -21,24 +21,23 @@ def gw(comp, u):
             return True
 
 
-print("comp Turn snake(s), water(w), gun(g)")
-r = random.randint(1, 3)
-if r == 1:
-    comp = "s"
-elif r == 2:
-    comp = "w"
-elif r == 3:
-    comp = "g"
+for i in range(10):
+    print("comp Turn snake(s), water(w), gun(g)")
+    r = random.randint(1, 3)
+    if r == 1:
+        comp = "s"
+    elif r == 2:
+        comp = "w"
+    elif r == 3:
+        comp = "g"
 
-u = input("Choose any one of this option s,w,g : ")
-a = gw(comp, u)
-print(f"computer chose {comp} and your chose {u}")
-if a is None:
-    print("The game is tie")
-elif a:
-    print("you win!")
-else:
-    print("you loss!")
-
-
+    u = input("Choose any one of this option s,w,g : ")
+    a = gw(comp, u)
+    print(f"computer chose {comp} and your chose {u}")
+    if a is None:
+        print("The game is tie")
+    elif a:
+        print("you win!")
+    else:
+        print("you loss!")
 
